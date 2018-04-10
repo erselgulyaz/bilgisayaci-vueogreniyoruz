b0f6cf13183fdacf1ce2498f1b4db4415da13ca6
erselgulyaz <erselgulyaz@gmail.com>
2018-04-11 00:52:32 +0300

    data, methods, computed, created, props başlıkları için çalışma yapıldı

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
   <div id="app">
     {{message}}
+    <hr>
+    <span>Toplam Ürün Sayısı: {{productTotal}}</span>
+    <button @click="addNewItem()">SEPETE EKLE</button>
+    <hr>
+    <hr>
+    <hr>
+    <hr>
+    <hr>
+    <hr>
+    <basic-component itemTitle="Başlık Alanı" itemContent="İçerik Alanı" />
   </div>
 </template>
 
 <script>
+import BasicComponent from './components/BasicComponent.vue'
 export default {
   data () {
     return {
-      message: 'Hello World!'
+      message: 'Hello World!',
+      products: [
+        {
+          name: 'Ürün 1'
+        }
+      ]
     }
+  },
+  methods: {
+    alertFunc () {
+      alert('Hello World!')
+    },
+    addNewItem () {
+      let item = {
+        name: 'Ürün x'
+      }
+      this.products.push(item)
+    }
+  },
+  computed: {
+    productTotal () {
+      return this.products.length
+    }
+  },
+  created () {
+    alert('sayfa yüklendi')
+  },
+  components: {
+    BasicComponent
   }
 }
 </script>
---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ itemTitle }}
+    <hr>
+    {{ itemContent }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemTitle: { type: String, required: true },
+    itemContent: { type: String }
+  }
+}
+</script>
+
+<style scoped>
+</style>
